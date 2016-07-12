ce6708b98a5109119f6690c33ca3725ec14d0dd5
vmatekole <victor@matekole.com>
2016-07-13 01:53:09 +0200

    if promo code fails enforce redirect to /checkout/payment not /checkout/payment/update

--- frontend/app/controllers/spree/store_controller.rb
+++ frontend/app/controllers/spree/store_controller.rb
@@ -18,21 +18,17 @@ module Spree
     end
 
     protected
-
-    # This method is placed here so that the CheckoutController
-    # and OrdersController can both reference it (or any other controller
-    # which needs it)
-    def apply_coupon_code
-      if params[:order] && params[:order][:coupon_code]
-        @order.coupon_code = params[:order][:coupon_code]
-
-        handler = PromotionHandler::Coupon.new(@order).apply
-
-        if handler.error.present?
-          flash.now[:error] = handler.error
-          respond_with(@order) { |format| format.html { render :edit } } and return
-        elsif handler.success
-          flash[:success] = handler.success
+      def apply_coupon_code
+        if params[:order] && params[:order][:coupon_code]
+          @order.coupon_code = params[:order][:coupon_code]
+          handler = PromotionHandler::Coupon.new(@order).apply
+          if handler.error.present?
+            flash.now[:error] = handler.error
+            @order.restart_checkout_flow
+            redirect_to checkout_state_path(@order.state) && return
+          elsif handler.success
+            flash[:success] = handler.success
+          end
         end
       end
     end
